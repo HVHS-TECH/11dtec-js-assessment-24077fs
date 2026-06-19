786f293b52ac518affca3acc931302cdec0f734b
24077fs <24077fs@hvhs.school.nz>
2026-06-19 17:02:47 +1200

    Update

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello world!")
+/****************************
+Js Assessment Finn Seiler
+****************************/
+const OUTPUT = document.getElementById("s4");
+
+
+
+console.log("Running Pristine Cuisine")
+/****************************
+Constants
+****************************/
+
+
+
+
+
+function Product(_name, _price){
+    PROOUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
+}
+
+
+Product("Bread", 2.99);
+
