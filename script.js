c6fbe4a1021271668de77ed27e641354f086d1da
24077fs <24077fs@hvhs.school.nz>
2026-06-25 13:39:26 +1200

    Update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 Js Assessment Finn Seiler
 ****************************/
 
-
+const OUTPUT = document.getElementById("Output");
 
 
 console.log("Running Pristine Cuisine")
@@ -21,5 +21,55 @@ function displayProduct(ID,_name, _price){
 
 }
 
+console.log(document.getElementById("s4"));
 
-displayProduct("s4","Eggs",5.99)
+displayProduct("s4","Pizza",4.99)
+displayProduct("s4","Pasta",4.99)
+displayProduct("s4","Lasanga",4.99)
+displayProduct("s4","Risotto",4.99)
+displayProduct("s4","Gelato",4.99)
+displayProduct("s5","Dumplings",4.99)
+displayProduct("s5","Fried Rice",4.99)
+displayProduct("s5","Peking Duck",4.99)
+displayProduct("s6","Tacos",4.99)
+displayProduct("s6","Burritos",4.99)
+displayProduct("s6","Enchiladas",4.99)
+displayProduct("s6","Gaucamole",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99)
+displayProduct("s","",4.99) 
+
+
+
+
+function getFormInput(){
+
+   const NAME_FIELD = document.getElementById("nameField");
+  var userName = NAME_FIELD.value;
+  OUTPUT.innerHTML = "<p>Hello " + userName + "</p>";
+}
