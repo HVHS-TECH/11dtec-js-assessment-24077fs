ea9828178295c9950aff5dce2b140985a9728d37
24077fs <24077fs@hvhs.school.nz>
2026-06-30 09:57:56 +1200

    Update

--- script.js
+++ script.js
@@ -12,11 +12,17 @@ Constants
 
 
 
+const items = [];
 
-
-function displayProduct(ID,_name, _price){
+function displayProduct(ID,name, price){
+   items.push({
+   name: name,
+   price: price
+   });
+   
+   
    const LISTID =  document.getElementById(ID)
-   LISTID.innerHTML +=  "<li>" + _name + " - $" + _price + "<button>Order Now!</button>" + "</li>";
+   LISTID.innerHTML +=  "<li>" + name + " - $" + price + "<button>Order Now!</button>" + "</li>";
 
 
 }
@@ -33,34 +39,33 @@ displayProduct("s6","Tacos",4.99)
 displayProduct("s6","Burritos",4.99)
 displayProduct("s6","Enchiladas",4.99)
 displayProduct("s6","Gaucamole",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99)
-displayProduct("s","",4.99) 
+displayProduct("s7","Sushi",4.99)
+displayProduct("s7","Ramen",4.99)
+displayProduct("s7","Tempura",4.99)
+displayProduct("s7","Udon",4.99)
+displayProduct("s9","Butter Chicken",4.99)
+displayProduct("s9","Curry",4.99)
+displayProduct("s9","Naan",4.99)
+displayProduct("s9","Biryani",4.99)
+displayProduct("s10","Crossant",4.99)
+displayProduct("s10","Bageutte",4.99)
+displayProduct("s10","Coq Au Vin",4.99)
+displayProduct("s10","Crème brûlée",4.99)
+displayProduct("s11","Shawarma",4.99)
+displayProduct("s11","Falafel",4.99)
+displayProduct("s11","Hummus",4.99)
+displayProduct("s11","Kababs",4.99)
+displayProduct("s12","Burger",4.99)
+displayProduct("s12","BBQ",4.99)
+displayProduct("s12","Fried Chicken",4.99)
+displayProduct("s12","Mac and Cheese",4.99)
+displayProduct("s13","Pad Thai",4.99)
+displayProduct("s13","Green Curry",4.99)
+displayProduct("s13","Tom Yum Soup",4.99)
+displayProduct("s14","Gyros",4.99)
+displayProduct("s14","Souvlaki",4.99)
+displayProduct("s14","Moussaka",4.99)
+displayProduct("s14","Tzatziki",4.99)
 
 
 
@@ -71,3 +76,6 @@ function getFormInput(){
   var userName = NAME_FIELD.value;
   OUTPUT.innerHTML = "<p>Hello " + userName + "</p>";
 }
+
+
+console.log(items);
