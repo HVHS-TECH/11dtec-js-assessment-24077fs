ac5d0a92dc8b2d8a27d313527fefc6d60119c294
24077fs <24077fs@hvhs.school.nz>
2026-07-24 18:09:40 +1200

    Final after rearrangeent and commenting

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
     /****************************
     Js Assessment Finn Seiler
     ****************************/
-    localStorage.clear();
+    
     const OUTPUT = document.getElementById("Output");
-    console.log(OUTPUT);
-
-    const items = [];
 
     console.log("Running Pristine Cuisine")
-    /****************************
-    Constants
-    ****************************/
 
     var receiptArray = JSON.parse(localStorage.getItem("cart")) || [];
-
-
-    function displayProduct(ID,name, price){
-    const LISTID =  document.getElementById(ID)
-    if(!LISTID){
-        return;
-    }
-
-    LISTID.innerHTML +=  "<li>" + name + " - $" + price + "<button onClick=\"getProduct('" + name + "', " + price + ")\" >Order Now!</button>" + "</li>";
-
-    }
 
     if (OUTPUT) {
         for (var i = 0; i < receiptArray.length; i++) {
@@ -36,47 +19,51 @@
         }
     }
 
+    function displayProduct(ID, name, price){    //Displays products using an id system and turning products listed in the js into js objects
+    const LISTID =  document.getElementById(ID)
+    if(!LISTID){
+        return;
+    }
+    LISTID.innerHTML +=  "<li>" + name + " - $" + price + "<button onClick=\"getProduct('" + name + "', " + price + ")\" >Order Now!</button>" + "</li>";
+    }
 
 
-
-    
-
-
-    function getProduct(name, price){
+    function getProduct(name, price){ //Pushes items ordered to an Array and Stringifies it for localstorage data transfer
     receiptArray.push({
     name: name,
     price: price
     });
-    console.log(receiptArray);
     localStorage.setItem("cart", JSON.stringify(receiptArray));
 
     console.log(receiptArray);
     
     }
 
-    function clearCart(){
+    function clearCart(){   //Clears cart completely
+        OUTPUT.innerHTML =  ""
         receiptArray = [];
         localStorage.removeItem("cart");
-        OUTPUT.innerHTML = "<h2>Receipt</h2>";
         console.log("Cart Cleared")
     }
 
 
 
-    function getFormInput(){
+    function getFormInput(){ //Gets form input from HTML and prints Reciept using the array stored in localstorage
         OUTPUT.innerHTML = "";
 
     const NAME_FIELD = document.getElementById("nameField");
     const MONEY_FIELD = document.getElementById("moneyField");
 
-    var userName = NAME_FIELD.value
-    var userMoney = Number(MONEY_FIELD.value);
+    var userName = (NAME_FIELD.value);
+    var userMoney = (MONEY_FIELD.value);
 
     var total = 0;
 
     for (var i = 0; i < receiptArray.length; i++){
 
+
     OUTPUT.innerHTML +=
+    
         receiptArray[i].name +
         " - $" +
         receiptArray[i].price +
@@ -85,9 +72,10 @@
         total += receiptArray[i].price;
     }
 
-    if (total > userMoney){
+    if (total > userMoney){ //The if loop used to tell if the person has enouht money or not.
     OUTPUT.innerHTML +="<h2>Hello " + userName + "</h2>" 
     OUTPUT.innerHTML += "<p>How dare you not enter enough money to pay.</p>"
+    return;
 
     } else {
 
@@ -95,21 +83,18 @@
 
     }
 
-
-
-
     console.log(total);
+    console.log(OUTPUT);
 
     OUTPUT.innerHTML += 
     "<h2>Hello " + userName + "</h2>" +
     "<p>Total: $" + total.toFixed(2) + "</p>" +
     "<p>Money: $" + userMoney.toFixed(2) + "</p>" +
     "<p>Change: $" + change.toFixed(2) + "</p>";
-
-
-
     }
 
+
+    //This abomination was used to display all the items on the menu every item is asigned to a section id. These id's are found on the HTML to put these items into a unordered list.
     displayProduct("s4","Pizza",4.99)
     displayProduct("s4","Pasta",4.99)
     displayProduct("s4","Lasanga",4.99)
