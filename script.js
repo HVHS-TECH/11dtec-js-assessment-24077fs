994e5a035248721d0ea60568b6a8ae77c8d17ed9
24077fs <24077fs@hvhs.school.nz>
2026-07-24 15:07:08 +1200

    Final Backup

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
     /****************************
     Js Assessment Finn Seiler
     ****************************/
-
+    localStorage.clear();
     const OUTPUT = document.getElementById("Output");
     console.log(OUTPUT);
 
@@ -39,45 +39,7 @@
 
 
 
-    displayProduct("s4","Pizza",4.99)
-    displayProduct("s4","Pasta",4.99)
-    displayProduct("s4","Lasanga",4.99)
-    displayProduct("s4","Risotto",4.99)
-    displayProduct("s4","Gelato",4.99)
-    displayProduct("s5","Dumplings",4.99)
-    displayProduct("s5","Fried Rice",4.99)
-    displayProduct("s5","Peking Duck",4.99)
-    displayProduct("s6","Tacos",4.99)
-    displayProduct("s6","Burritos",4.99)
-    displayProduct("s6","Enchiladas",4.99)
-    displayProduct("s6","Gaucamole",4.99)
-    displayProduct("s7","Sushi",4.99)
-    displayProduct("s7","Ramen",4.99)
-    displayProduct("s7","Tempura",4.99)
-    displayProduct("s7","Udon",4.99)
-    displayProduct("s9","Butter Chicken",4.99)
-    displayProduct("s9","Curry",4.99)
-    displayProduct("s9","Naan",4.99)
-    displayProduct("s9","Biryani",4.99)
-    displayProduct("s10","Crossant",4.99)
-    displayProduct("s10","Bageutte",4.99)
-    displayProduct("s10","Coq Au Vin",4.99)
-    displayProduct("s10","Crème brûlée",4.99)
-    displayProduct("s11","Shawarma",4.99)
-    displayProduct("s11","Falafel",4.99)
-    displayProduct("s11","Hummus",4.99)
-    displayProduct("s11","Kababs",4.99)
-    displayProduct("s12","Burger",4.99)
-    displayProduct("s12","BBQ",4.99)
-    displayProduct("s12","Fried Chicken",4.99)
-    displayProduct("s12","Mac and Cheese",4.99)
-    displayProduct("s13","Pad Thai",4.99)
-    displayProduct("s13","Green Curry",4.99)
-    displayProduct("s13","Tom Yum Soup",4.99)
-    displayProduct("s14","Gyros",4.99)
-    displayProduct("s14","Souvlaki",4.99)
-    displayProduct("s14","Moussaka",4.99)
-    displayProduct("s14","Tzatziki",4.99)
+    
 
 
     function getProduct(name, price){
@@ -125,7 +87,7 @@
 
     if (total > userMoney){
     OUTPUT.innerHTML +="<h2>Hello " + userName + "</h2>" 
-    OUTPUT.innerHTML += "<p>You fucking fool how dare you not enter enough money to pay for this amazing food that was manufacturered by the goblins of thee depths of the waiwhetu gold mine.</p>"
+    OUTPUT.innerHTML += "<p>How dare you not enter enough money to pay.</p>"
 
     } else {
 
@@ -147,3 +109,43 @@
 
 
     }
+
+    displayProduct("s4","Pizza",4.99)
+    displayProduct("s4","Pasta",4.99)
+    displayProduct("s4","Lasanga",4.99)
+    displayProduct("s4","Risotto",4.99)
+    displayProduct("s4","Gelato",4.99)
+    displayProduct("s5","Dumplings",4.99)
+    displayProduct("s5","Fried Rice",4.99)
+    displayProduct("s5","Peking Duck",4.99)
+    displayProduct("s6","Tacos",4.99)
+    displayProduct("s6","Burritos",4.99)
+    displayProduct("s6","Enchiladas",4.99)
+    displayProduct("s6","Gaucamole",4.99)
+    displayProduct("s7","Sushi",4.99)
+    displayProduct("s7","Ramen",4.99)
+    displayProduct("s7","Tempura",4.99)
+    displayProduct("s7","Udon",4.99)
+    displayProduct("s9","Butter Chicken",4.99)
+    displayProduct("s9","Curry",4.99)
+    displayProduct("s9","Naan",4.99)
+    displayProduct("s9","Biryani",4.99)
+    displayProduct("s10","Crossant",4.99)
+    displayProduct("s10","Bageutte",4.99)
+    displayProduct("s10","Coq Au Vin",4.99)
+    displayProduct("s10","Crème brûlée",4.99)
+    displayProduct("s11","Shawarma",4.99)
+    displayProduct("s11","Falafel",4.99)
+    displayProduct("s11","Hummus",4.99)
+    displayProduct("s11","Kababs",4.99)
+    displayProduct("s12","Burger",4.99)
+    displayProduct("s12","BBQ",4.99)
+    displayProduct("s12","Fried Chicken",4.99)
+    displayProduct("s12","Mac and Cheese",4.99)
+    displayProduct("s13","Pad Thai",4.99)
+    displayProduct("s13","Green Curry",4.99)
+    displayProduct("s13","Tom Yum Soup",4.99)
+    displayProduct("s14","Gyros",4.99)
+    displayProduct("s14","Souvlaki",4.99)
+    displayProduct("s14","Moussaka",4.99)
+    displayProduct("s14","Tzatziki",4.99)
